c81f2f166288a6b7da22187fb1ae2649b40b3a6e
j-verstaen <julietteverstaen@ucsb.edu>
2019-11-14 09:27:21 -0800

    updated quantified people figure

--- scripts/quantifying_people.R
+++ scripts/quantifying_people.R
@@ -10,7 +10,6 @@ library(googledrive)
 library(googlesheets4)
 library(dplyr)
 
-
 ##change this link to the real full one, cant read it in while people are currently working on it
 people <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/18xVyNCLBMPL9Z0uonMpJM5GxCn5fVP9pvFZPjp6YADU/edit#gid=1292190059",
                                     sheet = 2) 
@@ -28,7 +27,7 @@ quant_people <- people %>%
 packing <- circleProgressiveLayout(quant_people$num_ppl, sizetype='area')
 packing$radius <- 0.95*packing$radius
 data <- cbind(quant_people, packing) %>% 
-  mutate(id = seq(1:53))
+  mutate(id = seq(1:52))
 
 # The data used to create the circle polygons in ggplot need to have "Type" in the df in order to color the circles by type
 merge_id <- data %>% 
@@ -41,8 +40,21 @@ dat.gg <- dat.gg %>%
 
 # Plot 
 quant_ppl <- ggplot() + 
-  geom_polygon(data = dat.gg, aes(x, y, group = id, fill= `Type of Organization`), colour = "black", alpha = 0.6) +
-  geom_text(data = data, aes(x, y, label = `Short Name`, size = num_ppl), color="black") +
+  geom_polygon(data = dat.gg, aes(x, y, 
+                                  group = id, 
+                                  fill= `Type of Organization`), 
+               colour = "black", alpha = 0.6) +
+  geom_text(data = data, aes(x, y, 
+                             label = ifelse(`Short Name`== "Seafood Watch", "Seafood\nWatch",
+                                            ifelse(`Short Name`== "Ocean Conservancy", "Ocean\nConservancy",
+                                            ifelse(`Short Name`== "Wampanoag Tribe", "Wampanoag\nTribe",
+                                                   ifelse(`Short Name`== "CT Sea Grant", "CT\nSea Grant",
+                                                          ifelse(`Short Name` == "Cape Cod Coop Extension", "Cape Cod\nCoop Extension",
+                                                                 ifelse(`Short Name` == "Footprints in the water LLC", "Footprints in\nthe water LLC",
+                                                   `Short Name`)))))),
+                             #label = `Short Name`, 
+                             size = num_ppl), 
+            color="black") +
   theme_void() + 
  # theme(legend.position="none")+ 
   scale_fill_fish_d(option = "Thalassoma_bifasciatum" ) +
@@ -50,7 +62,7 @@ quant_ppl <- ggplot() +
   coord_equal()
 
 # Save the figure
-ggsave("figs/quant_ppl.jpg", width=7, height=5, dpi=300)
+ggsave("figs/quant_ppl.jpg", width=7, height=8, dpi=300)
 
 #fish_palettes()
 
