2cc7e20f325545d7a728c9218f1da0b6ed3d66a8
j-verstaen <julietteverstaen@ucsb.edu>
2020-04-19 14:30:17 -0700

    changed numeric range and reran dsc heatmaps

--- scripts/dsc_tweaks.R
+++ scripts/dsc_tweaks.R
@@ -16,16 +16,18 @@ dsc_scores_raw <- googlesheets4::read_sheet("https://docs.google.com/spreadsheet
 dsc_scores_goal <- dsc_scores_raw %>% 
   filter(Goal != "RES",
          Goal != "PRS") %>% 
-  unite(Go_lay, c(Goal,Layer), sep = ": ")
+  select(-Goal)
+   #unite(goa_lay, c(Goal,Layer), sep = ": ")
 
 ### create list for the proper order that we want it to show up on the graph later
 # layers order: from highest score to lowest
 order_layers_df <- dsc_scores_goal %>% 
-  select(Go_lay,`Average Score` ) %>%
+  select(Layer,`Average Score` ) %>%
+  #select(goa_lay,`Average Score` ) %>% 
   unique()  %>% 
   arrange(`Average Score`)
 
-order_layers <- c(order_layers_df$Go_lay)
+order_layers <- c(order_layers_df$Layer)
 
 # criteria order: same as in the table
 order_criteria <- c("Spatial Resolution", "Spatial Cover",
@@ -38,25 +40,27 @@ dsc_layers <- dsc_scores_goal %>%
   gather("criteria", "score", 2:9) %>% # Data is in long format, need it short
   mutate(score = as.numeric(score), #, na.rm = TRUE
          criteria = as.character(criteria),
-         Go_lay = as.character(Go_lay),
+         Layer = as.character(Layer),
          criteria = factor(criteria, levels = order_criteria),
-         Go_lay = factor(Go_lay, levels = order_layers)) 
+         Layer = factor(Layer, levels = order_layers)) 
+
 
 ### Heatmap!
-dsc_goal_layers <- ggplot(data = dsc_layers, aes(x = criteria, y = Go_lay)) +
+dsc_goal_layers <- ggplot(data = dsc_layers, aes(x = criteria, y = Layer)) +
   geom_tile(aes(fill = score)) +
-  scale_fill_gradient2(midpoint = 2, 
+  scale_fill_gradient2(midpoint = 0.5, 
                        low = "darkred", mid = "orange", high = "darkgreen", na.value = "grey80",
-                       breaks = c(1,2,3), 
+                       breaks = c(0,0.5,1), 
                        labels = c("Bad","Medium", "Good")) +
   theme_dark()+
   theme(axis.title = element_blank(),
         panel.grid = element_blank(),
         legend.title = element_blank(),
-        legend.position = "bottom",
-        axis.text.y = element_text(hjust = 0)) +
+        legend.position = "bottom") +
+      #  axis.text.y = element_text(hjust = 0)) +
   scale_x_discrete(labels = wrap_format(10))+
   coord_cartesian(expand=FALSE) 
 
 ## when saving make sure to save it tall enough so that there's no over lap for the yaxis labels
-ggsave("figs/dsc_heatmap_paper_option2.5.jpg", width=10, height=8, dpi=300)
+ggsave("figs/dsc_heatmap_paper_option1.jpg", width=10, height=8, dpi=300)
+
