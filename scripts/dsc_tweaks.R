267554df91c24357df8b96de9ae5f1b39d7a9789
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2021-02-28 18:38:52 -0800

    changed medium to fair

--- scripts/dsc_tweaks.R
+++ scripts/dsc_tweaks.R
@@ -11,23 +11,26 @@ library(dplyr)
 library(scales)
 
 ### read in the data from google sheets
-dsc_scores_raw <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1N2-B1KXB1w7rQYHV8Kl6ZmGK5jhE-JLIJk_VAPgyC60/edit#gid=271621882",
-                                            sheet = 2) 
+# dsc_scores_raw <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1N2-B1KXB1w7rQYHV8Kl6ZmGK5jhE-JLIJk_VAPgyC60/edit#gid=271621882",
+#                                             sheet = 2) 
+
+dsc_scores_raw <- read_csv("dqa_scores.csv")
+
 dsc_scores_goal <- dsc_scores_raw %>% 
   filter(Goal != "RES",
          Goal != "PRS") %>% 
-  select(-Goal)
-   #unite(goa_lay, c(Goal,Layer), sep = ": ")
+  #select(-Goal) %>%
+   unite(goa_lay, c(Goal,Layer), sep = ": ")
 
 ### create list for the proper order that we want it to show up on the graph later
 # layers order: from highest score to lowest
 order_layers_df <- dsc_scores_goal %>% 
-  select(Layer,`Average Score` ) %>%
-  #select(goa_lay,`Average Score` ) %>% 
+  #select(Layer,`Average Score` ) %>%
+  select(goa_lay,`Average Score` ) %>% 
   unique()  %>% 
   arrange(`Average Score`)
 
-order_layers <- c(order_layers_df$Layer)
+order_layers <- c(order_layers_df$goa_lay)
 
 # criteria order: same as in the table
 order_criteria <- c("Spatial Resolution", "Spatial Cover",
@@ -38,20 +41,21 @@ order_criteria <- c("Spatial Resolution", "Spatial Cover",
 dsc_layers <- dsc_scores_goal %>% 
   rename("Overall Score" = "Average Score") %>% 
   gather("criteria", "score", 2:9) %>% # Data is in long format, need it short
+  select(1,16,17) %>%
   mutate(score = as.numeric(score), #, na.rm = TRUE
          criteria = as.character(criteria),
-         Layer = as.character(Layer),
+         goa_lay = as.character(goa_lay),
          criteria = factor(criteria, levels = order_criteria),
-         Layer = factor(Layer, levels = order_layers)) 
+         goa_lay = factor(goa_lay, levels = order_layers)) 
 
 
 ### Heatmap!
-dsc_goal_layers <- ggplot(data = dsc_layers, aes(x = criteria, y = Layer)) +
+dsc_goal_layers <- ggplot(data = dsc_layers, aes(x = criteria, y = goa_lay)) +
   geom_tile(aes(fill = score)) +
   scale_fill_gradient2(midpoint = 0.5, 
                        low = "darkred", mid = "orange", high = "darkgreen", na.value = "grey80",
                        breaks = c(0,0.5,1), 
-                       labels = c("Bad","Medium", "Good")) +
+                       labels = c("Bad","Fair", "Good")) +
   theme_dark()+
   theme(axis.title = element_blank(),
         panel.grid = element_blank(),
@@ -62,5 +66,5 @@ dsc_goal_layers <- ggplot(data = dsc_layers, aes(x = criteria, y = Layer)) +
   coord_cartesian(expand=FALSE) 
 
 ## when saving make sure to save it tall enough so that there's no over lap for the yaxis labels
-ggsave("figs/dsc_heatmap_paper_option1.jpg", width=10, height=8, dpi=300)
+ggsave("figs/dsc_heatmap_paper_option2.jpg", width=10, height=8, dpi=300)
 
